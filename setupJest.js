6a642b33161e6662a32c05eed0d07733e180900d
Gerben <gerben@treora.com>
2017-07-19 19:47:12 +0200

    Add missing import, factor out check, tweak style.

--- setupJest.js
+++ setupJest.js
@@ -1,35 +1,42 @@
 import iface from 'jsdom/lib/jsdom/living/generated/Element'
+import NODE_TYPE from 'jsdom/lib/jsdom/living/node-type'
 
 Element.prototype.insertAdjacentElement = function insertAdjacentElement(position, node) {
-    position = position.toLowerCase();
-    
+    position = position.toLowerCase()
+
+    function checkHasValidParentNode(el) {
+        if (el.parentNode === null || el.parentNode.nodeType === NODE_TYPE.DOCUMENT_NODE) {
+            throw new DOMException(
+                DOMException.NO_MODIFICATION_ALLOWED_ERR,
+                "Cannot insert element adjacent to parent-less nodes or children of document nodes.",
+            )
+        }
+    }
+
     switch (position) {
         case "beforebegin": {
-            if (this.parentNode === null || this.parentNode.nodeType === NODE_TYPE.DOCUMENT_NODE) {
-                throw new DOMException(DOMException.NO_MODIFICATION_ALLOWED_ERR, "Cannot insert HTML adjacent to " +"parent-less nodes or children of document nodes.");
-            } else {
-                this.parentNode.insertBefore(node, this);
-            }
-            break;
+            checkHasValidParentNode(this)
+            this.parentNode.insertBefore(node, this)
+            break
         }
         case "afterend": {
-            if (this.parentNode === null || this.parentNode.nodeType === NODE_TYPE.DOCUMENT_NODE) {
-                throw new DOMException(DOMException.NO_MODIFICATION_ALLOWED_ERR, "Cannot insert HTML adjacent to " +"parent-less nodes or children of document nodes.");
-            } else {
-                this.parentNode.insertBefore(node, this.nextSibling);
-            }
-            break;
+            checkHasValidParentNode(this)
+            this.parentNode.insertBefore(node, this.nextSibling)
+            break
         }
         case "afterbegin": {
-            this.insertBefore(node, this.firstChild);
-            break;
+            this.insertBefore(node, this.firstChild)
+            break
         }
         case "beforeend": {
-            this.appendChild(node);
-            break;
+            this.appendChild(node)
+            break
         }
         default: {
-            throw new DOMException(DOMException.SYNTAX_ERR, "Must provide one of \"beforebegin\", \"afterend\", " +"\"afterbegin\", or \"beforeend\".");
+            throw new DOMException(
+                DOMException.SYNTAX_ERR,
+                'Must provide one of "beforebegin", "afterend", "afterbegin", or "beforeend".',
+            )
         }
     }
 }
