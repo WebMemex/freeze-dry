977bd33a88c6013e511333e29e574de8985631d2
Gerben <gerben@treora.com>
2018-04-24 15:48:15 +0200

    Tweak fix-links tests

--- src/fix-links.test.js
+++ src/fix-links.test.js
@@ -4,20 +4,29 @@ import fixLinks from './fix-links'
 
 
 describe('fixLinks', () => {
-    test('should do nothing for absolute URLs', async () => {
-        const rootElement = window.document.createElement('div')
-        rootElement.innerHTML = '<a href="https://example.com/#home">Link</a>'
-        await fixLinks({rootElement})
-        expect(rootElement.querySelector('*[href]').getAttribute('href'))
-            .toBe('https://example.com/#home')
-    })
-
     test('should make relative URLs absolute', async () => {
         const rootElement = window.document.createElement('div')
         rootElement.innerHTML = '<a href="otherpage#home">Link</a>'
         await fixLinks({rootElement})
         expect(rootElement.querySelector('*[href]').getAttribute('href'))
             .toBe('https://example.com/test/otherpage#home')
+        // note: window.document.URL is configured to be https://example.com/test/page
+    })
+
+    test('should take baseURI into account', async () => {
+        const rootElement = window.document.createElement('div')
+        rootElement.innerHTML = '<a href="otherpage#home">Link</a>'
+        await fixLinks({rootElement, baseURI: 'https://example.com/newpath/page'})
+        expect(rootElement.querySelector('*[href]').getAttribute('href'))
+            .toBe('https://example.com/newpath/otherpage#home')
+    })
+
+    test('should not alter absolute URLs', async () => {
+        const rootElement = window.document.createElement('div')
+        rootElement.innerHTML = '<a href="https://example.com/#home">Link</a>'
+        await fixLinks({rootElement})
+        expect(rootElement.querySelector('*[href]').getAttribute('href'))
+            .toBe('https://example.com/#home')
     })
 
     test('should not alter inline javascript in href attribute', async () => {
