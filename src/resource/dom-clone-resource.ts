b1fbf0f465bb6709b4bf0c179576cf3ee5042fe5
Gerben <gerben@treora.com>
2021-10-11 19:43:43 +0200

    Memoise cloned framed doc, allow passing orig frame

--- src/resource/dom-clone-resource.ts
+++ src/resource/dom-clone-resource.ts
@@ -7,8 +7,9 @@ import { HtmlDocumentLink } from '../extract-links/types'
 type DomCloneResourceConfig = Pick<GlobalConfig, 'glob' | 'getDocInFrame'>
 
 export class DomCloneResource extends DomResource {
-    private _originalDoc: Document
     protected _config: DomCloneResourceConfig
+    private _framesContentDocClones: Map<FrameElement, DomCloneResource | null>
+    private _originalDoc: Document
 
     /**
      * @param url - Since the passed Document already has a property doc.URL, the url parameter is optional; if
@@ -24,6 +25,7 @@ export class DomCloneResource extends DomResource {
         super(url, clone, config)
         this._config = config
         this._originalDoc = originalDoc
+        this._framesContentDocClones = new Map()
 
         // TODO Capture form input values (issue #19)
         // TODO Extract images from canvasses (issue #18)
@@ -37,6 +39,12 @@ export class DomCloneResource extends DomResource {
     getOriginalNode<T extends Node = Node>(nodeInClone: T) {
         const path = pathForDomNode(nodeInClone, this.doc)
         const originalNode = domNodeAtPath(path, this._originalDoc)
+        return originalNode as T
+    }
+
+    getClonedNode<T extends Node = Node>(nodeInOriginal: T) {
+        const path = pathForDomNode(nodeInOriginal, this._originalDoc)
+        const originalNode = domNodeAtPath(path, this.doc)
         return originalNode as T
     }
 
@@ -60,18 +68,47 @@ export class DomCloneResource extends DomResource {
         }
     }
 
-    getContentDocOfFrame(clonedFrameElement: FrameElement): DomCloneResource | null {
-        // TODO Memoise result in a map FrameElement→DomCloneResource
+    /**
+     * Get the clone of the framed Document for a given (i)frame element.
+     *
+     * As cloning a DOM does not clone the documents inside its frames (the `contentDocument` of a
+     * frame in the cloned resource is null), this method lets you obtain a clone of the framed
+     * document.
+     *
+     * On the first invocation, the frame content is cloned from the original document. Subsequent
+     * invocations will return this same object.
+     *
+     * @param frameElement The frame element for which to get the inner document
+     * @returns a clone of the document in the frame.
+     */
+    getContentDocOfFrame(frameElement: FrameElement): DomCloneResource | null {
+        // Look up the corresponding frame element in the other (original/cloned) doc.
+        let originalFrameElement, clonedFrameElement
+        if (frameElement.ownerDocument === this.doc) {
+            clonedFrameElement = frameElement
+            originalFrameElement = this.getOriginalNode(frameElement)
+        } else if (frameElement.ownerDocument === this._originalDoc) {
+            clonedFrameElement = this.getClonedNode(frameElement)
+            originalFrameElement = frameElement
+        } else {
+            throw new Error('Argument must be an element of either the original or the cloned document')
+        }
+
+        // Get/make the clone of the framed document.
+        let clonedInnerDoc = this._framesContentDocClones.get(clonedFrameElement)
+        if (clonedInnerDoc === undefined) {
+            // Make the clone and memoise it.
+            clonedInnerDoc = this._getContentDocOfFrame(originalFrameElement);
+            this._framesContentDocClones.set(clonedFrameElement, clonedInnerDoc)
+        }
+        return clonedInnerDoc
+    }
+
+    private _getContentDocOfFrame(originalFrameElement: FrameElement) {
         const { getDocInFrame = defaultGetDocInFrame } = this._config
-        const originalFrameElement = this.getOriginalNode(clonedFrameElement)
         const originalInnerDoc = getDocInFrame(originalFrameElement)
         if (originalInnerDoc !== null) {
-            const clonedInnerDoc = new DomCloneResource(
-                undefined,
-                originalInnerDoc,
-                this._config,
-            )
-            return clonedInnerDoc
+            return new DomCloneResource(undefined, originalInnerDoc, this._config)
         } else {
             return null
         }
